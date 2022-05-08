89713230a00394417b6669738f1bb307133c4ad0
Rodrigo Silva <rodsilvavieira@gmail.com>
2022-05-07 21:38:45 -0400

    feat: all icons for the application

---
+++ src/icons/pie-chart.tsx
@@ -0,0 +1,31 @@
+export function PieChartIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="23"
+      height="24"
+      viewBox="0 0 23 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M10.4068 4.01055C5.52282 4.01055 1.56055 7.97055 1.56055 12.8568C1.56055 17.7431 5.52055 21.7054 10.4068 21.7054C15.2931 21.7054 19.2531 17.7454 19.2531 12.8568H10.4068V4.01055V4.01055ZM21.4743 11.8441C21.4743 9.40207 20.4843 7.18993 18.8844 5.59L12.6281 11.8441H21.4743ZM11.5265 2.79297V11.637L17.7806 5.3829C16.0532 3.65552 13.7888 2.79297 11.5265 2.79297V2.79297Z"
+        fill="url(#paint0_linear_2_248)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_2_248"
+          x1="1.56055"
+          y1="12.2492"
+          x2="21.4743"
+          y2="12.2492"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#3374AB" />
+          <stop offset="0.598958" stopColor="#5AB1F3" />
+          <stop offset="1" stopColor="#6BD8FE" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
